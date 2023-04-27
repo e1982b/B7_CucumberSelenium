6dd75521a57ed2dbc01d01481aee2d84a89e391e
e1982b <e1982b@gmail.com>
2023-04-27 19:05:51 +0200

    All codes firstly coming

--- src/test/java/com/eurotech/utilities/aaa.java
+++ src/test/java/com/eurotech/utilities/aaa.java
@@ -3,5 +3,7 @@ package com.eurotech.utilities;
 public class aaa {
     public static void main(String[] args) {
         System.out.println("ho ho ho");
+        System.out.println("ho ho ho");
+        System.out.println("ho ho ho");
     }
 }
